6340a826bb4071d0b6b3e8c773889900b077d0de
papasavvasn <papasavvasn@mediamarktsaturn.com>
2024-12-20 14:59:44 +0100

    add server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const requestListener = function (req, res) {
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      console.log("Headers:", req.headers);
+      console.log("Body:", body);
+      res.writeHead(200);
+      res.end("Request logged");
+    });
+  } else {
+    res.writeHead(405);
+    res.end("Method Not Allowed");
+  }
+};
+
+const server = http.createServer(requestListener);
+server.listen(8000, () => {
+  console.log("Server is running on http://0.0.0.0:8000");
+});
