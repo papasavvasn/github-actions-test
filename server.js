5f483c7366127e4f915991de2b95f4b956027c90
papasavvasn <papasavvasn@mediamarktsaturn.com>
2024-12-23 16:55:36 +0100

    try to write to cache

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const requestListener = function (req, res) {
         const data = JSON.parse(body);
         console.log("data after parsing the body 2", data);
         const typeId = data.client_payload.typeId;
-        console.log("will store the object and cache", { "data.client_payload": data.client_payload });
+        console.log("will store the object and cache", "data.client_payload:", data.client_payload);
 
         // Store the object in the cache
         cache[typeId] = data.client_payload.text;
