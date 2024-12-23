39792fb798412060059e906c4faf42a2d70e9570
papasavvasn <papasavvasn@mediamarktsaturn.com>
2024-12-23 13:36:11 +0100

    add logs

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const http = require("http");
+const fs = require("fs");
+
+let cache = {};
+
+// Load cache from file if it exists
+if (fs.existsSync("cache.json")) {
+  cache = JSON.parse(fs.readFileSync("cache.json", "utf8"));
+}
 
 const requestListener = function (req, res) {
   console.log(`Received request: ${req.method} ${req.url}`);
@@ -10,8 +18,17 @@ const requestListener = function (req, res) {
     req.on("end", () => {
       console.log("Headers:", req.headers);
       console.log("Body:", body);
+      const data = JSON.parse(body);
+      const typeId = data.client_payload.typeId;
+
+      // Store the object in the cache
+      cache[typeId] = data;
+
+      // Save the cache to a file
+      fs.writeFileSync("cache.json", JSON.stringify(cache, null, 2));
+
       res.writeHead(200);
-      res.end("Request logged");
+      res.end("Request logged and processed");
     });
   } else {
     res.writeHead(405);
@@ -23,3 +40,22 @@ const server = http.createServer(requestListener);
 server.listen(8000, () => {
   console.log("Server is running on http://0.0.0.0:8000");
 });
+
+const contentfulContent = {
+  event_type: "contentful-sync",
+  client_payload: {
+    typeId: "cmsAboutUs",
+    text: [
+      {
+        id: "urlForBreadcrumb",
+        name: "urlForBreadcrumb",
+        type: "Text",
+        localized: false,
+        required: false,
+        validations: [],
+        disabled: false,
+        omitted: false,
+      },
+    ],
+  },
+};
